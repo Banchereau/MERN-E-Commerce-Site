07c345c4499ac42fda5aa57549838b33785a4eed
Banchereau <banchereau.jean-pierre@wanadoo.fr>
2022-11-21 15:41:26 +0100

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './pages/HomeScreen'
+import ProductScreen from './pages/ProductScreen'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />}></Route>
+              {/* <Route path='/login' element={<LoginScreen />}></Route> */}
+              <Route path='/product/:id' element={<ProductScreen />}></Route>
+              {/* <Route path='/cart' element={<CartScreen />}></Route>
+            <Route path='/cart/:id' element={<CartScreen />}></Route> */}
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   )
 }
---
+++ frontend/src/pages/ProductScreen.js
@@ -0,0 +1,189 @@
+import React, { useState } from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  ListGroupItem,
+  Form,
+} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products.js'
+
+const ProductScreen = () => {
+  const [quantity, setQuantity] = useState(1)
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const product = products.find((x) => x._id === id)
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?quantity=${quantity}`)
+  }
+  return (
+    <>
+      <Link className='btn btn-dark my-3 rounded' to='/'>
+        Go back
+      </Link>
+      <div>
+        <div className='hidden-xs'>
+          <Row>
+            <Col xl={6} lg={9}>
+              <div className='text-center'>
+                <Image src={product.image} alt={product.name} fluid />
+              </div>
+            </Col>
+            <Col xl={{ order: 2 }} lg={{ order: 3 }}>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <h3>{product.name}</h3>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroupItem>
+                <ListGroupItem>Price: ${product.price}</ListGroupItem>
+                <ListGroupItem>
+                  Description: ${product.description}
+                </ListGroupItem>
+              </ListGroup>
+            </Col>
+
+            <Col xl={{ order: 3 }} lg={{ order: 2 }}>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col className='my-auto'>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={quantity}
+                          onChange={(e) => setQuantity(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                <ListGroup.Item>
+                  <Button
+                    onClick={addToCartHandler}
+                    className='btn-block w-100'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </div>
+        <div className='hidden-lg'>
+          <Row>
+            <Col xs={12}>
+              <div className='text-center'>
+                <Image src={product.image} alt={product.name} fluid />
+              </div>
+            </Col>
+            <Col xs={12}>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <h3>{product.name}</h3>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroupItem>
+                <ListGroupItem>Price: ${product.price}</ListGroupItem>
+                <ListGroupItem>
+                  Description: ${product.description}
+                </ListGroupItem>
+              </ListGroup>
+            </Col>
+
+            <Col xs={12}>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col className='my-auto'>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={quantity}
+                          onChange={(e) => setQuantity(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                <ListGroup.Item>
+                  <Button
+                    onClick={addToCartHandler}
+                    className='btn-block w-100'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProductScreen
